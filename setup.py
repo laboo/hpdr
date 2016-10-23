44a3ed1b67536f764f99ac78a1796685e0324777
Mark Libucha <mlibucha@gmail.com>
2016-10-22 18:55:26 -0700

    Added install_requires.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='hpdr',
     packages=find_packages(),
-    version='0.1',
+    version='0.2',
     description='Hive partition date range',
     author='Mark Libucha',
     author_email='mlibucha@gmail.com',
@@ -26,5 +26,17 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: Implementation :: CPython'
-    )
+    ),
+    install_requires=[
+        "attrs>=16.1.0, <17.0.0",
+        "enum34>=1.1.6, <2.0.0",
+        "funcparserlib>=0.3.6, <1.0.0",
+        "future>=0.15.2, <1.0.0",
+        "pendulum>=0.5.5, <1.0.0",
+        "pytest>=3.0.2, <4.0.0",
+        "python-dateutil>=2.5.3, <3.0.0",
+        "pytz>=2016.6.1, <2017.0.0",
+        "six>=1.10.0, <2.0.0",
+        "tabulate>=0.7.5, <2.0.0"
+    ]
 )
