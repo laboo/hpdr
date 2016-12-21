6abd05eed7dc2f012cd0a261033149f0f222beb2
Mark Libucha <mlibucha@gmail.com>
2016-12-20 18:04:08 -0800

    Fix for build with steps when there's no slop.

--- hpdr/api.py
+++ hpdr/api.py
@@ -25,27 +25,30 @@ def build(begin,
           minutes='MIN'):
     """Build a specification for a date range.
 
-    :param begin: begin date of range, a datetime or yyyymmddmmss string
-    :param end: end date of range, a datetime or yyyymmddmmss string
-    :param izone: (optional) time zone for input dates, defaults to UTC
-    :param qzone: (optional) time zone to use in query, defaults to UTC
-    :param slop: (optional) duration to add to both ends of the partition range,
-                            specified as \\d+[years|months|days|hours|minutes],
-                            for example, 5hours
-    :param lslop: (optional) duration to add to the front end of the partition range,
-                            specified as \\d+[years|months|days|hours|minutes],
-                            for example, 5hours
-    :param rslop: (optional) duration to add to the back end of the partition range,
-                            specified as \\d+[years|months|days|hours|minutes],
-                            for example, 5hours
-    :param years: (optional) name for years partition, defaults to YYYY
-    :param months: (optional) name for months partition, defaults to MM
-    :param days: (optional) name for days partition, defaults to DD
-    :param hours: (optional) name for hours partition, defaults to HH
-    :param minutes: (optional) name for hours partition, defaults to MIN
-    :return: :class:`Spec` object representing the date range
-    :rtype: :class:`Spec`
+    Args:
+        begin: begin date of range, a datetime or yyyy[mm[dd[mm[ss]]]] string
+        end: end date of range, a datetime or yyyy[mm[dd[mm[ss]]]] string
+        izone: (optional) time zone for input dates, defaults to UTC
+        qzone: (optional) time zone to use in query, defaults to UTC
+        slop: (optional) duration to add to both ends of the partition range,
+            specified as \\d+[years|months|days|hours|minutes],
+            for example, 5hours
+        lslop: (optional) duration to add to the front end of the partition range,
+            specified as \\d+[years|months|days|hours|minutes],
+            for example, 5hours
+        rslop: (optional) duration to add to the back end of the partition range,
+            specified as \\d+[years|months|days|hours|minutes],
+            for example, 5hours
+        years: (optional) name for years partition, defaults to YYYY
+        months: (optional) name for months partition, defaults to MM
+        days: (optional) name for days partition, defaults to DD
+        hours: (optional) name for hours partition, defaults to HH
+        minutes: (optional) name for hours partition, defaults to MIN
+
+    Returns: 
+       A Spec object representing the date range
     """
+
     keywords = {}
     if izone:
         keywords['izone'] = izone
@@ -97,28 +100,48 @@ def build_with_steps(begin,
                      days='DD',
                      hours='HH',
                      minutes='MIN'):
-    """Build a specification for a date range.
+    """Build a lists of specification for a date.
 
-    :param begin: begin date of range, a datetime or yyyymmddmmss string
-    :param end: end date of range, a datetime or yyyymmddmmss string
-    :param izone: (optional) time zone for input dates, defaults to UTC
-    :param qzone: (optional) time zone to use in query, defaults to UTC
-    :param slop: (optional) duration to add to both ends of the partition range,
-                            specified as \\d+[years|months|days|hours|minutes],
-                            for example, 5hours
-    :param lslop: (optional) duration to add to the front end of the partition range,
-                            specified as \\d+[years|months|days|hours|minutes],
-                            for example, 5hours
-    :param rslop: (optional) duration to add to the back end of the partition range,
-                            specified as \\d+[years|months|days|hours|minutes],
-                            for example, 5hours
-    :param years: (optional) name for years partition, defaults to YYYY
-    :param months: (optional) name for months partition, defaults to MM
-    :param days: (optional) name for days partition, defaults to DD
-    :param hours: (optional) name for hours partition, defaults to HH
-    :param minutes: (optional) name for hours partition, defaults to MIN
-    :return: :class:`Spec` object representing the date range
-    :rtype: :class:`Spec`
+    The specifications in the list are contiguous, chronological pieces of the list.
+    Left slop followed by the begin-to-end range broken into parts of step size
+    followed by right slop.
+
+    Args:
+        begin: begin date of range, a datetime or yyyy[mm[dd[mm[ss]]]] string
+        end: end date of range, a datetime or yyyy[mm[dd[mm[ss]]]] string
+        step: (optional) duration to break individual Spec objects into,
+            specified as \\d+[years|months|days|hours|minutes],
+            for example, 5hours. If None, one Spec is returned.
+        izone: (optional) time zone for input dates, defaults to UTC
+        qzone: (optional) time zone to use in query, defaults to UTC
+        slop: (optional) duration to add to both ends of the partition range,
+            specified as \\d+[years|months|days|hours|minutes],
+            for example, 5hours
+        lslop: (optional) duration to add to the front end of the partition range,
+            specified as \\d+[years|months|days|hours|minutes],
+            for example, 5hours
+        rslop: (optional) duration to add to the back end of the partition range,
+            specified as \\d+[years|months|days|hours|minutes],
+            for example, 5hours
+        years: (optional) name for years partition, defaults to YYYY
+        months: (optional) name for months partition, defaults to MM
+        days: (optional) name for days partition, defaults to DD
+        hours: (optional) name for hours partition, defaults to HH
+        minutes: (optional) name for hours partition, defaults to MIN
+
+    Returns: 
+       A list of Spec objects representing the date range. For example,
+
+       build_with_steps(begin='20160901', end=20161001, step=10days, --slop=1hours)
+
+       returns a list of five Spec objects, representing these ranges:
+
+       (YYYY=2016 AND MM=08 AND DD=31 AND HH>=23) [left slop of 1 hour]
+       (YYYY=2016 AND MM=09 AND DD>=01 AND DD<11) [10 days]
+       (YYYY=2016 AND MM=09 AND DD>=11 AND DD<21) [10 days]
+       (YYYY=2016 AND MM=09 AND DD>=21)           [10 days]
+       (YYYY=2016 AND MM=10 AND DD=01 AND HH=00)  [right slop of 1 hour]
+    
     """
 
     keywords = {}
@@ -152,12 +175,15 @@ def build_with_steps(begin,
         keywords['slop'] = keywords['lslop'] = keywords['rslop'] = None
         if not isinstance(step, timedelta):
             step = utils.deltastr_to_td(step)
-        specs.append(Spec(main_spec.slop_begin, main_spec.begin, **keywords))
+        if main_spec.slop_begin < main_spec.begin:
+            specs.append(Spec(main_spec.slop_begin, main_spec.begin, **keywords))
         walk_begin = main_spec.begin
         while walk_begin < main_spec.end:
             walk_end = min(main_spec.end, walk_begin + step)
             specs.append(Spec(walk_begin, walk_end, **keywords))
             walk_begin = walk_end
-        specs.append(Spec(main_spec.end, main_spec.slop_end, **keywords))
+
+        if main_spec.end < main_spec.slop_end:
+            specs.append(Spec(main_spec.end, main_spec.slop_end, **keywords))
 
     return specs
