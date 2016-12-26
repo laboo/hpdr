cc30d85eab704c03d6813635b78299cbd1f5a1c5
Mark Libucha <mlibucha@gmail.com>
2016-12-26 14:13:20 -0800

    Upped version to 0.8.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='hpdr',
     packages=find_packages(),
-    version='0.7',
+    version='0.8',
     description='Hive date ranges simplified.',
     author='Mark Libucha',
     author_email='mlibucha@gmail.com',
